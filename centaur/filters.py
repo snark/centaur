f09f8f09076cde1ef1569d65f7c9a35f94326f3b
Steve Cook <scook@saymedia.com>
2013-05-06 20:50:07 -0400

    More filter functions

--- centaur/filters.py
+++ centaur/filters.py
@@ -1,4 +1,49 @@
 import re
+import datetime
+
+
+def any_of(**kwargs):
+    filters = kwargs['filters']
+    # NB
+    # When we begin inflating filters out of configs, we'll want
+    # to inflate each filter here.
+    def filter(entry):
+        filter_passed = False
+        for f in filter:
+            if f(entry):
+                filter_passed = entry
+                break
+        return filter_passed
+    return filter
+
+
+def all_of(**kwargs):
+    filters = kwargs['filters']
+    # NB
+    # When we begin inflating filters out of configs, we'll want
+    # to inflate each filter here.
+    def filter(entry):
+        filter_passed = False
+        for f in filter:
+            entry = f(entry)
+            if not entry:
+                break
+        return entry
+    return filter
+
+
+def normalize_time(**kwargs):
+    def filter(entry):
+        if 'created_parsed' in entry:
+            c = entry['created_parsed'][0:6]
+            created = dateime.datetime(*c)
+            entry['created'] = created.strftime('%Y-%m-%dT%H:%M:%SZ')
+        if 'updated_parsed' in entry:
+            u = entry['updated_parsed'][0:6]
+            updated = datetime.datetime(*u)
+            entry['updated'] = updated.strftime('%Y-%m-%dT%H:%M:%SZ')
+        return entry
+    return filter
 
 
 def title_matches(**kwargs):
