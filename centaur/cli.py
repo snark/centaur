96b51ed0399f1b162251eb838ce3a26763526369
Josh Kaderlan <jkaderlan@yahoo.com>
2013-07-29 14:42:27 -0700

    pep8 cleanup
    now all tests pass under Python 3!

--- centaur/cli.py
+++ centaur/cli.py
@@ -9,9 +9,9 @@ from centaur import (config, parser, util)
 def main():
     argparser = argparse.ArgumentParser(description='')
     argparser.add_argument('config', nargs='+',
-        help='full path to config file')
-    argparser.add_argument('-f', '--feed', action='append', 
-            help='a feed to include')
+                           help='full path to config file')
+    argparser.add_argument('-f', '--feed', action='append',
+                           help='a feed to include')
     args = argparser.parse_args()
     cnf = {}
     try:
@@ -20,9 +20,9 @@ def main():
             for key in config_dict:
                 if key not in cnf:
                     cnf[key] = config_dict[key]
-                elif key !='feeds' and cnf[key] != config_dict[key]:
+                elif key != 'feeds' and cnf[key] != config_dict[key]:
                     sys.exit('You specified multiple actions for {}! '
-                        'Please specify only one.'.format(key))
+                             'Please specify only one.'.format(key))
                 elif key == 'feeds':
                     cnf[key].extend(config_dict[key])
     except Exception as e:
@@ -41,14 +41,15 @@ def main():
             sys.exit('Could not inflate filters!\n{}'.format(e))
     try:
         aggregators = [
-            util.inflate_aggregator(k, v) for k, v in cnf['aggregators'].items()
+            util.inflate_aggregator(k, v)
+            for k, v in cnf['aggregators'].items()
         ]
     except Exception as e:
         sys.exit('Could not inflate aggregators!\n{}'.format(e))
     try:
         for a in aggregators:
             # Pump to prime
-            next(a) 
+            next(a)
         for f in cnf['feeds']:
             parser.parse_feed(f, aggregators=aggregators, filters=filters)
         for a in aggregators:
@@ -58,4 +59,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
